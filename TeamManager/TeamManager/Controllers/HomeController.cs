b81935e0a1733c76efa71a1694cabea94f69cd6e
fangliping <fangliping56@gmail.com>
2014-02-18 13:42:12 +0800

    init code

---
+++ TeamManager/TeamManager/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace TeamManager.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [OutputCache(NoStore = true, Duration = 0)]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(Models.Users user, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.IsValid(user.UserID, user.Password))
+                {                    
+                    FormsAuthentication.SetAuthCookie(user.UserID, false);
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+    
+}
